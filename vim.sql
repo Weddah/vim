20ff68b3b0199e0caa824ec5b85238bc75543086
Weddah <139496361+Weddah@users.noreply.github.com>
2023-08-03 17:21:16 +0300

    Add files via upload

--- vim.sql
+++ vim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2021 at 04:22 PM
+-- Generation Time: Aug 03, 2023 at 04:15 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -41,6 +41,75 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
 (1, 'admin', '5c428d8875d2948607f3e3fe134d71b4', '2017-10-30 11:42:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbookings`
+--
+
+CREATE TABLE `tblbookings` (
+  `EventType` varchar(100) NOT NULL,
+  `Venue` varchar(100) NOT NULL,
+  `Stime` time NOT NULL,
+  `Etime` time NOT NULL,
+  `Date` date NOT NULL,
+  `Description` varchar(100) NOT NULL,
+  `Status` varchar(100) NOT NULL,
+  `IsRead` varchar(50) NOT NULL,
+  `empid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblclients`
+--
+
+CREATE TABLE `tblclients` (
+  `EmpId` int(11) NOT NULL,
+  `FirstName` varchar(30) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `EmailId` varchar(30) NOT NULL,
+  `Password` varchar(40) NOT NULL,
+  `Phonenumber` int(15) NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `Gender` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblclients`
+--
+
+INSERT INTO `tblclients` (`EmpId`, `FirstName`, `LastName`, `EmailId`, `Password`, `Phonenumber`, `Status`, `Gender`) VALUES
+(90, 'Ken', 'Muciri', 'ken@gmail.com', '1e9e9a787de1a1a5d4508bf9b1ed3175', 723456798, '1', 'Male'),
+(101, 'Leila', 'Khalid', 'leila@gmail.com', '1f7ff802df649315948fcdf33e87a86a', 721908765, '1', 'Female'),
+(45, 'Keith ', 'Seth', 'keith@gmail.com', '8c8b7878db796ff68adf8e578005486f', 754327689, '1', 'Male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbleventype`
+--
+
+CREATE TABLE `tbleventype` (
+  `EventType` varchar(100) NOT NULL,
+  `Description` varchar(100) NOT NULL,
+  `Sprice` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbleventype`
+--
+
+INSERT INTO `tbleventype` (`EventType`, `Description`, `Sprice`) VALUES
+('Live Media Coverage', '1 Hour Session', 35650),
+('Cinematography', 'Motion-Picture Photography', 45900),
+('Podcast', 'A feature on a podcast', 24500),
+('Photography', 'Product/Photoshoot', 35540),
+('Blog', 'A blog feature', 67890),
+('Social Media Campaign', '3 Weekly posts, a Reel, Short and Tiktok Video', 98500),
+('Vlog', 'A vlog feature', 78450);
 
 --
 -- Indexes for dumped tables
